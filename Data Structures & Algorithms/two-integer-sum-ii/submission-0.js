0892456d5f738ebcf135cd735288b1f2531e7c31
Joshua Martinez <pruncle21@gmail.com>
2026-08-16 11:33:59 -0500

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        const map = new Map();
+
+        for (let i = 0; i < numbers.length; i++) {
+            if (map.has(target - numbers[i])) {
+                return [map.get(target - numbers[i]) + 1, i + 1];
+            } else {
+                map.set(numbers[i], i);
+            }
+        }
+
+        return [];
+    }
+}
